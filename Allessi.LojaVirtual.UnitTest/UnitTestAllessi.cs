17e7cc6412baf1a387478651e1c5d045ad35314e
allessi <allessi.si@gmail.com>
2015-11-24 23:20:58 -0200

    Primeiro commit

---
+++ Allessi.LojaVirtual.UnitTest/UnitTestAllessi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Allessi.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestAllessi
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
